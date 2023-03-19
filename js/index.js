4bf5e0b37cbc209990c58b5f36bc1e27c4c75b93
SAIKAT <mohammadislam.snhu@gmail.com>
2023-03-19 08:19:17 +0400

    feat: add text editor

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 const openMenu = getElement('.text-editor--header-open-menu');
 const closeMenu = getElement('.text-editor--header-close-menu');
 const mainMenu = getElement('.text-editor--header-main-menu');
-
+const boldText = getElement('.bold-txt');
+const italicText = getElement('.italic-txt');
+const underlineText = getElement('.underline-txt');
+const textBox = getElement('#input-text');
+console.log(italicText)
 addListener(openMenu, 'click', function () {
     mainMenu.style.left = 0;
     closeMenu.style.display = 'block';
@@ -15,6 +19,26 @@ addListener(closeMenu, 'click', function () {
     openMenu.style.display = 'block';
 })
 
+addListener(boldText, 'click',function () {
+    boldText.style.backgroundColor = 'black';
+    boldText.style.color = 'white';
+    textBox.style.fontWeight = '900';
+
+})
+
+addListener(italicText, 'click',function () {
+    italicText.style.backgroundColor = 'black';
+    italicText.style.color = 'white';
+    textBox.style.fontStyle = 'italic';
+    
+})
+
+addListener(underlineText, 'click',function () {
+    underlineText.style.backgroundColor = 'black';
+    underlineText.style.color = 'white';
+    textBox.style.textDecoration = 'underline';
+    
+})
 
 function getElement(id) {
     return document.querySelector(id);
